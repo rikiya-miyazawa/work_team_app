ba3e3d17aab047d94b9d617840262606965b3415
qz <meshipsmou@yahoo.co.jp>
2023-02-07 17:00:32 +0900

    user_new作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        
+      else
+        render :new
+      end
   end
 
-  def edit
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
